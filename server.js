d99af0f6ba5f6a34831e05671a245e5f1646b92d
Luiz Eduardo <tutoriais.gamehelp@gmail.com>
2021-11-07 23:28:04 -0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const port = process.env.PORT || 9000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -9,4 +9,5 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 9000);
+console.log('[Express] Creating a new server na porta: ' + port)
+app.listen(port);
