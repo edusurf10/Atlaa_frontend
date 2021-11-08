dab9af0849df59906368e36b993539c1d31baeac
Luiz Eduardo <tutoriais.gamehelp@gmail.com>
2021-11-07 23:19:29 -0300

    First Commit

---
+++ build/precache-manifest.0707f01ee1f47932fb162fdf750a795e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810d2e9681f9cc563653d9e825710731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eb4b49914407fb9bd98",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "11c047bda4f5ae1efd54",
+    "url": "/static/css/main.165214d4.chunk.css"
+  },
+  {
+    "revision": "1eb4b49914407fb9bd98",
+    "url": "/static/js/2.17e2af7b.chunk.js"
+  },
+  {
+    "revision": "ad496997e43fef12ee7b6a3c2baeae36",
+    "url": "/static/js/2.17e2af7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c047bda4f5ae1efd54",
+    "url": "/static/js/main.321b8a3f.chunk.js"
+  },
+  {
+    "revision": "4641ae4fbfd043621c57",
+    "url": "/static/js/runtime-main.c8afb56d.js"
+  },
+  {
+    "revision": "ef1125c36f6985c9bb7e2fa1330066c0",
+    "url": "/static/media/email.ef1125c3.png"
+  },
+  {
+    "revision": "6baed521ce486d9581b5c9b6a705ad41",
+    "url": "/static/media/fb.6baed521.png"
+  },
+  {
+    "revision": "52b31710b9c029e06c5d9a2418e90f61",
+    "url": "/static/media/icon_nickname.52b31710.png"
+  },
+  {
+    "revision": "0f0b27be05d3d689c2958c8ceb5ad1c5",
+    "url": "/static/media/icon_senha.0f0b27be.png"
+  },
+  {
+    "revision": "0c81915ea1cc98c1dd187d797820de85",
+    "url": "/static/media/logo_gif.0c81915e.gif"
+  }
+]);
